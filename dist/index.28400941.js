3ab2bb002c9c900914273472c58593a2af021c65
Qendresa Shala <alesa@live.se>
2023-02-01 12:08:01 +0100

    created a function show all movies

--- dist/index.28400941.js
+++ dist/index.28400941.js
@@ -1,14 +1,20 @@
-const charactersBox = document.querySelector("#characters");
-const showButton = document.querySelector("show-button");
-const urlCharacter = "http https://swapi.dev/api/people/";
-async function getAllNames() {
-    const response = await fetch(urlCharacter);
-    const listOfCharacters = await response.json();
-    return listOfCharacters;
-} // showButton.addEventListener("click", async (event) =>
- //     event.preventDefault();
- // getRandomName().then((facts) => {
- //     charactersBox.innerHTML = data.name;
- // });
+const movieSection = document.querySelector("#movie-section");
+const submitButton = document.querySelector("submit-button");
+const urlMovies = "https://swapi.dev/api/films/";
+async function getMovies() {
+    const response = await fetch(urlMovies);
+    const dataMovies = await response.json();
+    return dataMovies;
+}
+function showMovies() {
+    getMovies().then((dataMovies)=>{
+        for(let i = 0; i < dataMovies.results.length; i++){
+            const movieTitle = document.createElement("p");
+            movieTitle.innerHTML = `${dataMovies.results[i].title}`;
+            movieSection.append(movieTitle);
+        }
+    });
+}
+showMovies();
 
 //# sourceMappingURL=index.28400941.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,18 +1,23 @@
-const charactersBox = document.querySelector("#characters") as HTMLElement;
-const showButton = document.querySelector("show-button") as HTMLButtonElement;
+const movieSection = document.querySelector("#movie-section") as HTMLElement;
+const submitButton = document.querySelector("submit-button") as HTMLButtonElement;
 
-const urlCharacter = "http https://swapi.dev/api/people/";
+const urlMovies = "https://swapi.dev/api/films/";
 
-async function getAllNames() {
-    const response = await fetch(urlCharacter);
-    const listOfCharacters = await response.json();
-    return listOfCharacters;
+async function getMovies() {
+    const response = await fetch(urlMovies);
+    const dataMovies = await response.json();
+    return dataMovies;
 }
 
-// showButton.addEventListener("click", async (event) =>
-//     event.preventDefault();
+function showMovies() {
+    getMovies().then((dataMovies) => {
+        for (let i = 0; i < dataMovies.results.length; i++) {
+            const movieTitle = document.createElement('p');
+            movieTitle.innerHTML = `${dataMovies.results[i].title}`;
+            movieSection.append(movieTitle);
 
-// getRandomName().then((facts) => {
-//     charactersBox.innerHTML = data.name;
+        }
+    });
+};
 
-// });
+showMovies();
