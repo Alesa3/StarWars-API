10db3ff3da2579332692ed2bd354dd7ceaa5906e
Qendresa Shala <alesa@live.se>
2023-02-01 16:19:29 +0100

    characters being shown

--- dist/index.28400941.js
+++ dist/index.28400941.js
@@ -1,7 +1,11 @@
 const movieSection = document.querySelector("#movie-section");
 const submitButton = document.querySelector("submit-button");
 let unorderList = document.getElementById("unorder-list");
+const openingMovie = document.getElementById("openingMovie");
+const characterInput = document.getElementById("char-input");
+const displayFact = document.querySelector(".display-fact");
 const urlMovies = "https://swapi.dev/api/films/";
+const urlPlanets = "https://swapi.dev/api/planets/:";
 async function getMovies() {
     const response = await fetch(urlMovies);
     const dataMovies = await response.json();
@@ -12,12 +16,33 @@ function showMovies() {
         for(let i = 0; i < dataMovies.results.length; i++){
             const movieTitle = document.createElement("li");
             movieTitle.innerHTML = `${dataMovies.results[i].title}`;
+            const cardButton = document.createElement("button");
+            cardButton.className = "showAll";
+            cardButton.innerHTML = "See more";
             // movieSection.append(movieTitle);
             unorderList.append(movieTitle);
             movieSection.append(unorderList);
+            movieTitle.append(cardButton);
         }
+        const aboutButton = document.getElementsByClassName("showAll");
+        for(let i = 0; i < aboutButton.length; i++)aboutButton[i].addEventListener("click", function() {
+            openingMovie.innerHTML = `${dataMovies.results[i].opening_crawl}`;
+        });
     });
 }
-showMovies();
+showMovies(); // async function getPlanet(id: string) {
+ //     const response = await fetch(urlPlanets + id);
+ //     const characterInfo = await response.json();
+ //     return characterInfo;
+ // }
+ // submitButton.addEventListener("click", (event) => {
+ //     event.preventDefault();
+ //     if (characterInput.value.length > 0) {
+ //         getPlanet(characterInput.value).then((characterInfo) => {
+ //             displayFact.innerHTML = characterInfo.name;
+ //             characterInput.value = "";
+ //         });
+ //     }
+ // });
 
 //# sourceMappingURL=index.28400941.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,29 @@
 const movieSection = document.querySelector("#movie-section") as HTMLElement;
 const submitButton = document.querySelector("submit-button") as HTMLButtonElement;
 let unorderList = document.getElementById('unorder-list') as HTMLUListElement;
+const openingMovie = document.getElementById('openingMovie') as HTMLParagraphElement;
+const characterInput = document.getElementById('char-input') as HTMLInputElement;
+const displayFact = document.querySelector(".display-fact") as HTMLParagraphElement;
 
+type StarWarsMovie = {
+    title: string,
+    episode_id: number,
+    opening_crawl: string
+}
 
+type Character = {
+    name: string,
+    birth_year: number | string,
+    eye_color: string,
+    gender: string,
+    hair_color: string,
+    height: string,
 
+}
 
 const urlMovies = "https://swapi.dev/api/films/";
+const urlPlanets = "https://swapi.dev/api/planets/:";
+
 
 async function getMovies() {
     const response = await fetch(urlMovies);
@@ -19,14 +37,44 @@ function showMovies() {
             const movieTitle = document.createElement('li');
 
             movieTitle.innerHTML = `${dataMovies.results[i].title}`;
+            const cardButton = document.createElement('button');
+            cardButton.className = "showAll";
+            cardButton.innerHTML = "See more";
             // movieSection.append(movieTitle);
             unorderList.append(movieTitle);
             movieSection.append(unorderList);
-
-
-
+            movieTitle.append(cardButton);
         }
+
+        const aboutButton = document.getElementsByClassName("showAll");
+        for (let i = 0; i < aboutButton.length; i++) {
+            aboutButton[i].addEventListener("click", function () {
+                openingMovie.innerHTML = `${dataMovies.results[i].opening_crawl}`;
+
+            });
+        };
+
     });
 };
-
 showMovies();
+
+// async function getPlanet(id: string) {
+//     const response = await fetch(urlPlanets + id);
+//     const characterInfo = await response.json();
+//     return characterInfo;
+// }
+
+// submitButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+
+//     if (characterInput.value.length > 0) {
+//         getPlanet(characterInput.value).then((characterInfo) => {
+//             displayFact.innerHTML = characterInfo.name;
+//             characterInput.value = "";
+//         });
+
+//     }
+
+
+// });
+
