a251765dbeb64c9e4efabd299398557cd02a2f3b
Qendresa Shala <alesa@live.se>
2023-02-03 16:02:57 +0100

    trying out a new search

--- dist/index.28400941.js
+++ dist/index.28400941.js
@@ -1,11 +1,18 @@
 const movieSection = document.querySelector("#movie-section");
+const selectMovie = document.querySelector("#select-movie");
 const submitButton = document.querySelector(".submit-button");
 let unorderList = document.getElementById("unorder-list");
 let twoUnorderList = document.getElementById("two-unorder-list");
 const characterInput = document.getElementById("char-input");
 const openingCrawl = document.querySelector("#opening-crawl");
+const displayName = document.querySelector(".display-name");
+const birthYear = document.querySelector(".birth-year");
+const eyeColor = document.querySelector(".eye-color");
+const displayGender = document.querySelector(".display-gender");
+const hairColor = document.querySelector(".hair-color");
+const displayHeight = document.querySelector(".display-height");
 const urlMovies = "https://swapi.dev/api/films/";
-const urlCharacter = "https://swapi.dev/api/people/";
+const urlCharacter = "https://swapi.dev/api/people/?search=";
 async function getMovies() {
     const response = await fetch(urlMovies);
     const dataMovies = await response.json();
@@ -25,42 +32,29 @@ function showMovies() {
             movieTitle.append(cardButton);
         }
         const aboutButton = document.getElementsByClassName("showAll");
-        for(let i = 0; i < aboutButton.length; i++)// const movieTitleTwo = document.createElement('li');
-        // const movieDirector = document.createElement('li');
-        // const movieProd = document.createElement('li');
-        aboutButton[i].addEventListener("click", async function() {
+        for(let i = 0; i < aboutButton.length; i++)aboutButton[i].addEventListener("click", async function() {
             openingCrawl.innerHTML = `${dataMovies.results[i].opening_crawl}`;
-        // movieTitleTwo.innerHTML = ` Title: ${dataMovies.results[i].title}`;
-        // movieDirector.innerHTML = `Director: ${dataMovies.results[i].director}`;
-        // movieProd.innerHTML = `Producer: ${dataMovies.results[i].producer}`;
-        // openingCrawl.innerHTML = `Story: ${dataMovies.results[i].opening_crawl}`;
-        // twoUnorderList.append(movieTitleTwo, openingCrawl, movieDirector, movieProd);
         });
     });
 }
 showMovies();
-const displayName = document.querySelector(".display-name");
-const birthYear = document.querySelector(".birth-year");
-const eyeColor = document.querySelector(".eye-color");
-const displayGender = document.querySelector(".display-gender");
-const hairColor = document.querySelector(".hair-color");
-const displayHeight = document.querySelector(".display-height");
-async function getCharacter(id) {
-    const response = await fetch(urlCharacter + id);
-    console.log(urlCharacter);
+async function getCharacter(search) {
+    const response = await fetch(urlCharacter + characterInput);
+    console.log(urlCharacter + characterInput.value);
     const characterInfo = await response.json();
     return characterInfo;
 }
 submitButton.addEventListener("click", async (event)=>{
     event.preventDefault();
+    console.log(urlCharacter + characterInput);
     if (characterInput.value.length > 0) getCharacter(characterInput.value).then((characterInfo)=>{
-        displayName.innerHTML = `Name: ${characterInfo.name}`;
+        console.log(characterInfo.results[0].name);
+        displayName.innerHTML = `Name: ${characterInfo.results[0].name}`;
         birthYear.innerHTML = `Birth Year: ${characterInfo.birth_year}`;
         eyeColor.innerHTML = `Eye color: ${characterInfo.eye_color}`;
         displayGender.innerHTML = `Gender: ${characterInfo.gender}`;
         hairColor.innerHTML = `Hair color: ${characterInfo.hair_color}`;
         displayHeight.innerHTML = `Height: ${characterInfo.height}`;
-        characterInput.value = "";
     });
 });
 
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,20 +1,22 @@
 const movieSection = document.querySelector("#movie-section") as HTMLElement;
+const selectMovie = document.querySelector("#select-movie") as HTMLElement;
 const submitButton = document.querySelector(".submit-button") as HTMLButtonElement;
 let unorderList = document.getElementById('unorder-list') as HTMLUListElement;
 let twoUnorderList = document.getElementById('two-unorder-list') as HTMLUListElement;
 const characterInput = document.getElementById('char-input') as HTMLInputElement;
-
 const openingCrawl = document.querySelector("#opening-crawl") as HTMLParagraphElement;
+
+const displayName = document.querySelector(".display-name") as HTMLParagraphElement;
+const birthYear = document.querySelector(".birth-year") as HTMLParagraphElement;
+const eyeColor = document.querySelector(".eye-color") as HTMLParagraphElement;
+const displayGender = document.querySelector(".display-gender") as HTMLParagraphElement;
+const hairColor = document.querySelector(".hair-color") as HTMLParagraphElement;
+const displayHeight = document.querySelector(".display-height") as HTMLParagraphElement;
+
 
 
 type StarWarsMovie = {
-
-    title: string,
     opening_crawl: string
-    release_date: number,
-    director: string,
-    producer: string,
-
 }
 
 type Character = {
@@ -27,7 +29,7 @@ type Character = {
 }
 
 const urlMovies = "https://swapi.dev/api/films/";
-const urlCharacter = "https://swapi.dev/api/people/";
+const urlCharacter = "https://swapi.dev/api/people/?search=";
 
 
 async function getMovies() {
@@ -51,23 +53,12 @@ function showMovies() {
             movieTitle.append(cardButton);
         }
 
+
         const aboutButton = document.getElementsByClassName("showAll");
+
         for (let i = 0; i < aboutButton.length; i++) {
-            // const movieTitleTwo = document.createElement('li');
-
-            // const movieDirector = document.createElement('li');
-            // const movieProd = document.createElement('li');
-
             aboutButton[i].addEventListener("click", async function () {
                 openingCrawl.innerHTML = `${dataMovies.results[i].opening_crawl}`;
-                // movieTitleTwo.innerHTML = ` Title: ${dataMovies.results[i].title}`;
-                // movieDirector.innerHTML = `Director: ${dataMovies.results[i].director}`;
-                // movieProd.innerHTML = `Producer: ${dataMovies.results[i].producer}`;
-                // openingCrawl.innerHTML = `Story: ${dataMovies.results[i].opening_crawl}`;
-
-                // twoUnorderList.append(movieTitleTwo, openingCrawl, movieDirector, movieProd);
-
-
             });
         };
 
@@ -76,37 +67,31 @@ function showMovies() {
 showMovies();
 
 
-const displayName = document.querySelector(".display-name") as HTMLParagraphElement;
-const birthYear = document.querySelector(".birth-year") as HTMLParagraphElement;
-const eyeColor = document.querySelector(".eye-color") as HTMLParagraphElement;
-const displayGender = document.querySelector(".display-gender") as HTMLParagraphElement;
-const hairColor = document.querySelector(".hair-color") as HTMLParagraphElement;
-const displayHeight = document.querySelector(".display-height") as HTMLParagraphElement;
 
-
-
-async function getCharacter(id: string) {
-    const response = await fetch(urlCharacter + id);
-    console.log(urlCharacter);
+async function getCharacter(search: any) {
+    const response = await fetch(urlCharacter + characterInput);
+    console.log(urlCharacter + characterInput.value)
     const characterInfo = await response.json();
     return characterInfo;
 }
 
-
 submitButton.addEventListener("click", async (event) => {
     event.preventDefault();
+    console.log(urlCharacter + characterInput)
 
 
     if (characterInput.value.length > 0) {
         getCharacter(characterInput.value).then((characterInfo) => {
-            displayName.innerHTML = `Name: ${characterInfo.name}`;
+            console.log(characterInfo.results[0].name);
+            displayName.innerHTML = `Name: ${characterInfo.results[0].name}`;
             birthYear.innerHTML = `Birth Year: ${characterInfo.birth_year}`;
             eyeColor.innerHTML = `Eye color: ${characterInfo.eye_color}`;
             displayGender.innerHTML = `Gender: ${characterInfo.gender}`;
             hairColor.innerHTML = `Hair color: ${characterInfo.hair_color}`;
             displayHeight.innerHTML = `Height: ${characterInfo.height}`;
-            characterInput.value = "";
 
         });
     }
 });
+
+
