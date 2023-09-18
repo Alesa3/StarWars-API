9ac2df3f9a2c35f485ab05f5912ec3c2d8ea1550
Alesa <alesa@live.se>
2023-09-18 20:53:00 +0200

    new heart function

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -48,7 +48,6 @@ type Planet = {
 }
 
 
-/* Function to fetch all movie titles. While pressing on them, more info displayed on another section */
 
 async function getMovies() {
     const response = await fetch(urlMovies);
@@ -58,32 +57,94 @@ async function getMovies() {
 
 function showMovies() {
     getMovies().then((dataMovies) => {
-        for (let i = 0; i < dataMovies.results.length; i++) {
-            const movieTitle = document.createElement('li');
-            movieTitle.innerHTML = `${dataMovies.results[i].title}`;
-            const cardButton = document.createElement('button');
-            cardButton.className = "showAll";
-            cardButton.innerHTML = "See more";
-            unorderList.append(movieTitle);
-            movieSection.append(unorderList);
-            movieTitle.append(cardButton);
+      for (let i = 0; i < dataMovies.results.length; i++) {
+        const movieTitle = document.createElement('li');
+        movieTitle.innerHTML = `${dataMovies.results[i].title}`;
+        
+        const cardButton = document.createElement('button');
+        cardButton.className = "showAll";
+        cardButton.innerHTML = "See more";
+        movieTitle.appendChild(cardButton);
+        
+        const heartIcon = document.createElement('span');
+        heartIcon.className = "heart-icon";
+        heartIcon.innerHTML = "&#x2661;"; 
+        movieTitle.appendChild(heartIcon);
+        
+        unorderList.append(movieTitle);
+        movieSection.append(unorderList);
+      }
+  
+      const aboutButton = document.getElementsByClassName("showAll");
+      const heartIcons = document.getElementsByClassName("heart-icon");
+      const movieHeader = document.createElement('h5');
+  
+      for (let i = 0; i < aboutButton.length; i++) {
+        aboutButton[i].addEventListener("click", async function () {
+          movieHeader.innerHTML = `${dataMovies.results[i].title}`;
+          openingCrawl.innerHTML = `${dataMovies.results[i].opening_crawl}`;
+          openingCrawl.append(movieHeader);
+        });
+  
+        heartIcons[i].addEventListener("click", function () {
+          toggleHeartIcon(i);
+          saveToFavorites(dataMovies.results[i].title);
+        });
+      }
+  
+      function toggleHeartIcon(index: number) {
+        const heartIcons = document.getElementsByClassName("heart-icon");
+        const icon = heartIcons[index];
+  
+        if (icon.classList.contains("filled")) {
+          icon.classList.remove("filled");
+          icon.innerHTML = "&#x2661;";
+          removeFavorite(dataMovies.results[index].title);
+        } else {
+          icon.classList.add("filled");
+          icon.innerHTML = "&#x2665;"; 
+          addFavorite(dataMovies.results[index].title);
         }
+      }
+  
+      function addFavorite(movieTitle: string) {
+        favorites.push(movieTitle);
+        console.log("Added to favorites:", movieTitle);
+        saveToFavorites(favorites);
+        displayFavorites();
+      }
+  
+      function removeFavorite(movieTitle: string) {
+        const index = favorites.indexOf(movieTitle);
+        if (index > -1) {
+          favorites.splice(index, 1);
+          console.log("Removed from favorites:", movieTitle);
+          saveToFavorites(favorites);
+            displayFavorites();
+        }
+      }
 
-        const aboutButton = document.getElementsByClassName("showAll");
-        const movieHeader = document.createElement('h5');
+      function saveToFavorites(favorites: any) {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+      }
 
-        for (let i = 0; i < aboutButton.length; i++) {
-            aboutButton[i].addEventListener("click", async function () {
-                movieHeader.innerHTML = `${dataMovies.results[i].title}`;
-                openingCrawl.innerHTML = `${dataMovies.results[i].opening_crawl}`;
-                openingCrawl.append(movieHeader)
+      function displayFavorites() {
+        const favoritesList = document.getElementById("favorites-list") as HTMLElement;
+        favoritesList.innerHTML = "";
 
-            });
-        };
+        for (let i = 0; i < favorites.length; i++) {
+          const favoriteItem = document.createElement('li');
+          favoriteItem.innerHTML = favorites[i];
+          favoritesList.appendChild(favoriteItem);
+        }
+      }
     });
-}
+  }
 
-showMovies();
+  const favorites: string[] = [];
+
+  showMovies();
+
 
 
 /*-- Search button that takes user input and returns correct information --*/
@@ -113,34 +174,39 @@ submitButton.addEventListener("click", async (event) => {
     }
 });
 
+
 /*--- get Planets ---*/
 
-async function getPlanets() {
-    const response = await fetch(urlPlanetsOne);
-    const factsPlanet = await response.json();
-    return factsPlanet;
-}
+// async function getPlanets() {
+//     const response = await fetch(urlPlanetsOne);
+//     const factsPlanet = await response.json();
+//     return factsPlanet;
+// }
 
-function getPlanetFact() {
-    getPlanets().then((factsPlanet) => {
+// function getPlanetFact() {
+//     getPlanets().then((factsPlanet) => {
 
-        tatoPlanet.innerHTML = `<li>${factsPlanet.results[0].name} 
-        is the home of Luke Skywalker and the first planet ever visited in Star Wars.</li>`;
+        // tatoPlanet.innerHTML = `<li>${factsPlanet.results[0].name} 
+        // is the home of Luke Skywalker and the first planet ever visited in Star Wars.</li>`;
 
-        dagoPlanet.innerHTML = `<li>${factsPlanet.results[4].name} 
-        is the disgusting swamp planet where Luke Skywalker seeks out the training of Yoda. 
-        <br>This planet may seem like the perfect hiding spot for Yoda because it's one massive swamp, 
-        but the actual reason is actually much more nuanced than that.</li>`;
+        // dagoPlanet.innerHTML = `<li>${factsPlanet.results[4].name} 
+        // is the disgusting swamp planet where Luke Skywalker seeks out the training of Yoda. 
+        // <br>This planet may seem like the perfect hiding spot for Yoda because it's one massive swamp, 
+        // but the actual reason is actually much more nuanced than that.</li>`;
 
-        yavinPlanet.innerHTML = `<li>${factsPlanet.results[2].name} 
-        is the fourth moon of the planet Yavin. 
-        The moon is completely covered in jungles, making it a perfect home for the Rebel Base in A New Hope.</li>`
+        // yavinPlanet.innerHTML = `<li>${factsPlanet.results[2].name} 
+        // is the fourth moon of the planet Yavin. 
+        // The moon is completely covered in jungles, making it a perfect home for the Rebel Base in A New Hope.</li>`
 
-        mustaPlanet.innerHTML = `<li>${factsPlanet.results[9].name} 
-        is famous for being the location of Anakin Skywalker's duel with his old master,
-        Obi-Wan Kenobi. <br> It's certainly a striking location for a duel, seeing as its surface is 35% lava.</li>`
+        // mustaPlanet.innerHTML = `<li>${factsPlanet.results[9].name} 
+        // is famous for being the location of Anakin Skywalker's duel with his old master,
+        // Obi-Wan Kenobi. <br> It's certainly a striking location for a duel, seeing as its surface is 35% lava.</li>`
 
-    })
-};
+//     })
+// };
 
-getPlanetFact();
+// getPlanetFact();
+// function saveToFavorites(title: any) {
+//     throw new Error("Function not implemented.");
+// }
+
